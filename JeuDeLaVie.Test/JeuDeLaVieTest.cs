9bdb2fd074f6865a4af0cf670ae616a754949976
Enzo Sandré <atelier@enzosandre.fr>
2022-11-14 14:48:55 +0100

    Premier test rouge

--- JeuDeLaVie.Test/JeuDeLaVieTest.cs
+++ JeuDeLaVie.Test/JeuDeLaVieTest.cs
@@ -2,10 +2,19 @@ namespace JeuDeLaVie.Test
 {
     public class JeuDeLaVieTest
     {
-        [Fact]
+        [Fact(DisplayName = "ETANT DONNE une cellule vivante " +
+              "QUAND on passe un tour " +
+              "ALORS elle est morte")]
         public void Test1()
         {
+            // ETANT DONNE une cellule vivante
+            var cellule = new Cellule(vivante: true);
 
+            // QUAND on passe un tour
+            cellule.PasserUnTour();
+
+            // ALORS elle est morte
+            Assert.False(cellule.EstVivante);
         }
     }
 }
